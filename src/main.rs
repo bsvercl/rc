b6ce29e76b66b046f2b3b07b54fbff3789bd97b0
Brad Svercl <bradsvercl@gmail.com>
2017-04-08 22:34:28 -0500

    add text

--- src/main.rs
+++ src/main.rs
@@ -37,12 +37,24 @@ fn main() {
         .unwrap();
     let mut counter = FPSCounter::new();
 
+    let mut glyphs = Glyphs::new("assets/fonts/InputMono-Regular.ttf", window.factory.clone())
+        .unwrap();
+
     while let Some(e) = window.next() {
         match e {
             Input::Update(args) => app.update(args.dt),
             Input::Render(_) => {
-                window.draw_2d(&e, |c, g| app.render(&c, g)).unwrap();
-                window.set_title(format!("rc | fps: {}", counter.tick()));
+                window
+                    .draw_2d(&e, |c, g| {
+                        app.render(&c, g);
+                        text([1.0, 0.0, 0.0, 1.0],
+                             11,
+                             &format!("fps: {}", counter.tick()),
+                             &mut glyphs,
+                             c.trans(10.0, 1.0).transform,
+                             g);
+                    })
+                    .unwrap();
             }
             Input::Press(Button::Keyboard(key)) => app.handle_key(key, true),
             Input::Release(Button::Keyboard(key)) => app.handle_key(key, false),
